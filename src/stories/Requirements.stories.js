76cab23376ef5c9c5821034c7bde4b93a9f51b0c
Orivelton Cesar <orivelton10@gmail.com>
2021-03-19 09:31:41 +0000

    chore: controls

--- src/stories/Requirements.stories.js
+++ src/stories/Requirements.stories.js
@@ -5,14 +5,10 @@ import { MapPortugalSvg } from '../components/MapPortugalSvg';
 const stories = storiesOf('App Test', module);
 
 stories.add('App', () => {
-  const [fill, setFill] = useState('#FFFFFF');
 
   return (
     <>
-      <label htmlFor="color">Color: </label>
-      <input id="color" type="color" value={fill} onChange={e => setFill(e.target.value)}/>
-      <hr />
-      <MapPortugalSvg  {...{ fill, hoverCircle: 'blue' }}/>
+      <MapPortugalSvg  {...{ hoverCircle: 'blue' }}/>
     </>
   );
 });
